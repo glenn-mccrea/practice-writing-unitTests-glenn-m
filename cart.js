0e031aa52431b9c381ecec6fa598616e364c5b49
Glenn McCrea <glennmccrea@icloud.com>
2025-09-10 16:35:00 -0500

    2 tests working

--- cart.js
+++ cart.js
@@ -1,13 +1,15 @@
-const cart = [];
+let cart = [];
 
 function addItem(cart, item, quantity) {
   cart.push({ item, quantity });
+  return cart;
 }
 
 function removeItem(cart, item) {
   const index = cart.findIndex((cartItem) => cartItem.name === item.name);
   if (index !== -1) {
     cart.splice(index, 1);
+    return cart;
   }
 }
 
--- tests/cart.test.js
+++ tests/cart.test.js
@@ -1,9 +1,25 @@
-const cart = require("../cart.js");
+const cartTest = require("../cart.js");
+
+let cart = [];
 
 describe("addItem", function () {
   test("Should add an item to the specified cart in the quantity input.", function () {
-    expect(cart.addItem(cart, "pants", 2)).toBe(`[
-  { item: 'pants', quantity: 5 }
-]`);
+    expect(cartTest.addItem(cart, "pants", 2)).toStrictEqual([
+      { item: "pants", quantity: 2 },
+    ]);
+  });
+});
+
+describe("removeItem", function () {
+  test("Should remove items from the cart", function () {
+    expect(cartTest.removeItem(cart, "pants")).toStrictEqual([]);
+  });
+});
+
+describe("getTotalItems", function () {
+  test("Should add an item to the specified cart in the quantity input.", function () {
+    expect(cartTest.addItem(cart, "pants", 2)).toBe(`[
+      { item: "pants", quantity: 2 },
+    ]`);
   });
 });
