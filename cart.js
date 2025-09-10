29c380600207e74a64e1bda287f6904ddebe9af1
Glenn McCrea <glennmccrea@icloud.com>
2025-09-09 22:08:02 -0500

    created modules and test folder

---
+++ cart.js
@@ -0,0 +1,22 @@
+const cart = [];
+
+function addItem(cart, item, quantity) {
+  cart.push({ item, quantity });
+}
+
+function removeItem(cart, item) {
+  const index = cart.findIndex((cartItem) => cartItem.name === item.name);
+  if (index !== -1) {
+    cart.splice(index, 1);
+  }
+}
+
+function getTotalItems(cart) {
+  return;
+  cart.reduce(
+    (total, cartItem) => total + cartItem.quantity,
+    0 /*init accumlulator to zero*/
+  );
+}
+
+module.exports = { addItem, removeItem, getTotalItems };
